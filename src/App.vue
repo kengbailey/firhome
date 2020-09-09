4c1bd8e2f22cc8c94d3ed58405a423610935892a
Ken Bailey <jamdat33@gmail.com>
2020-09-09 11:23:33 -0500

    initial commit; waverunner ui

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,123 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-file-video</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Wav Runner</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Fir</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-col>
+          <v-row
+           justify="center" 
+           align="center"
+          >
+            <v-form>
+              <v-text-field label="Youtube URL">
+              </v-text-field>
+            </v-form>
+            <v-btn class="ml-4" @click="submit">go</v-btn>
+          </v-row>
+          <v-row
+           justify="center" 
+           align="center"
+          >
+            <v-data-table
+              :headers="headers"
+              :items="songs"
+              :items-per-page="10"
+              class="elevation-4">
+            </v-data-table>
+          </v-row>
+        </v-col>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      headers: [
+        {
+          text: 'Song Title',
+          align: 'start',
+          sortable: false,
+          value: 'title'
+        },
+        { text: 'Link', value: 'link'},
+        { text: 'Download', value: 'dl'},
+        { text: 'Date', value: 'date'},
+      ],
+      songs: [
+        {title: 'Young Thug - Boolin', link: 'google.com', dl: 'yesh1', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Coolin', link: 'google.com', dl: 'yesh2', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Goonin', link: 'google.com', dl: 'yesh3', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Vroomin', link: 'google.com', dl: 'yesh4', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Droolin', link: 'google.com', dl: 'yesh5'},
+        {title: 'Young Thug - Boolin', link: 'google.com', dl: 'yesh1'},
+        {title: 'Young Thug - Coolin', link: 'google.com', dl: 'yesh2'},
+        {title: 'Young Thug - Goonin', link: 'google.com', dl: 'yesh3', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Vroomin', link: 'google.com', dl: 'yesh4'},
+        {title: 'Young Thug - Droolin', link: 'google.com', dl: 'yesh5'},
+        {title: 'Young Thug - Boolin', link: 'google.com', dl: 'yesh1'},
+        {title: 'Young Thug - Coolin', link: 'google.com', dl: 'yesh2'},
+        {title: 'Young Thug - Goonin', link: 'google.com', dl: 'yesh3', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Vroomin', link: 'google.com', dl: 'yesh4'},
+        {title: 'Young Thug - Droolin', link: 'google.com', dl: 'yesh5', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Boolin', link: 'google.com', dl: 'yesh1', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Coolin', link: 'google.com', dl: 'yesh2', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Goonin', link: 'google.com', dl: 'yesh3', date: new Date().toLocaleString()},
+        {title: 'Young Thug - Vroomin', link: 'google.com', dl: 'yesh4'},
+        {title: 'Young Thug - Droolin', link: 'google.com', dl: 'yesh5'},
+      ]
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+    methods: {
+      submit () {
+        this.$v.$touch()
+      },
+    }
+  }
+</script>
